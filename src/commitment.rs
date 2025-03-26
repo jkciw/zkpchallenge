150fdbc5b90c9e3b8dbc06a754dc600595f4e19b
BEULAHEVANJALIN <beulahebenezer777@gmail.com>
2025-03-26 19:47:15 +0530

    fix Pedersen commitment

--- src/commitment.rs
+++ src/commitment.rs
@@ -12,34 +12,32 @@ pub struct PedersenCommitment {
 
 fn get_generator_h(secp: &Secp256k1<All>, generator_g: &PublicKey) -> Result<PublicKey, Box<dyn Error>> {
     let hash = sha256::Hash::hash(&generator_g.serialize());
-    let h_sk = SecretKey::from_slice(&hash.to_byte_array())?;
-    let generator_h = PublicKey::from_secret_key(secp, &h_sk);
+    let h_scalar = Scalar::from_be_bytes(hash.to_byte_array()).map_err(|_| "Invalid scalar for H")?;
+    let generator_h = generator_g.mul_tweak(secp, &h_scalar.into())?;
     Ok(generator_h)
 }
 
-fn scalar_from_u64(value: u64) -> Result<SecretKey, Box<dyn Error>> {
-    // u64 to 8 little-endian bytes and pad the rest with zeros
+fn scalar_from_u64(value: u64) -> Result<Scalar, Box<dyn Error>> {
     let mut bytes = [0u8; 32];
     bytes[..8].copy_from_slice(&value.to_le_bytes());
-    let sk = SecretKey::from_slice(&bytes)?;
-    Ok(sk)
+    let scalar = Scalar::from_be_bytes(bytes).map_err(|_| "Invalid scalar")?;
+    Ok(scalar)
 }
 
 fn generate_commitment(secp: &Secp256k1<All>, value: u64, blinding: &SecretKey) -> Result<PublicKey, Box<dyn Error>> {
-    let a_sk = scalar_from_u64(value)?;
+    let value_scalar = scalar_from_u64(value)?;
     
     let secret_g = SecretKey::from_slice(&[1; 32])?;
     let generator_g = PublicKey::from_secret_key(secp, &secret_g);
 
-    // independent generator from G.
     let generator_h = get_generator_h(secp, &generator_g)?;
     
-    let a_g = generator_g.mul_tweak(secp, &a_sk.into())?;
-    
+    let v_g = generator_g.mul_tweak(secp, &value_scalar)?;
+
     let blinding_scalar = Scalar::from_be_bytes(blinding.secret_bytes()).map_err(|_| "Invalid scalar")?;
     let r_h = generator_h.mul_tweak(secp, &blinding_scalar)?;
     
-    let commitment = a_g.combine(&r_h)?;
+    let commitment = v_g.combine(&r_h)?;
     
     Ok(commitment)
 }
